3388ca31590cb77a3e1452db71d787df2f8405b5
Elijah Josh V. Opriasa <202510253@gordoncollege.edu.ph>
2026-08-03 10:20:18 +0800

    slice and includes method

--- FREECODECAMP/workshops/workshop5/w5.js
+++ FREECODECAMP/workshops/workshop5/w5.js
@@ -1,8 +1,24 @@
 // Building a String inspector
 // includes() and slice() 
 
-const fccSentence = "freeCodeCamp is a great place to learn web development.";
-console.log("Here are some examples of the includes() method:");
+const message = "Welcome to freeCodeCamp!";
+const fccSentence = "freeCodeCamp";
 
-const hasFreeCodeCamp = fccSentence.includes(`free`);
-console.log(hasFreeCodeCamp);
+const greetingWord = message.slice(0, 7)
+const platform = message.slice(11, 23)
+const endPunctuation = message.slice(-1)
+const hasFreeCodeCamp = fccSentence.includes("freeCodeCamp");
+const hasJavaScript = fccSentence.includes("JavaScript");
+const hasLowercaseFCC = fccSentence.includes("freecodecamp");
+
+
+console.log(`fccSentence.includes("freeCodeCamp") returns ${hasFreeCodeCamp} because the word "freeCodeCamp" is in the sentence.`);
+console.log(`fccSentence.includes("JavaScript") returns ${hasJavaScript} because the word "JavaScript" is not in the sentence.`);
+console.log(`fccSentence.includes("freecodecamp") returns ${hasLowercaseFCC} because includes is case-sensitive.`);
+console.log("Here are some examples of the slice() method:")
+
+console.log(`The word "freeCodeCamp" was sliced from the message.`)
+console.log(`The first word is "${greetingWord}".`);
+console.log(`The ending punctuation mark is a "${endPunctuation}"`);
+
+console.log("Workshop complete! You now know how to use includes() and slice().")
